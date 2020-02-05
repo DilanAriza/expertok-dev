26f418efed25167228f25bef14b3dbd280eaa90e
Dilan Santiago Ariza Cañon <52083149+DilanAriza@users.noreply.github.com>
2020-02-05 17:26:51 -0500

    Arreglo 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import $ from 'jquery';
+
+import './css/index.css';
+
 import {
     BrowserRouter as Router,
     Switch,
@@ -15,41 +18,45 @@ import Home from './Home';
 function App() {
     return (
         <Router>
-            <nav className="navbar navbar-expand-lg navbar-light bg-white">
-                <Link to="/" className="display-4 mb-3 text-primary text-decoration-none">Expertok</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span className="navbar-toggler-icon"></span> </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent1">
-                    <ul className="navbar-nav ml-auto mr-2">
-                        <li className="nav-item active"> 
-                            <Link className="nav-link" to="/">Inicio</Link> 
-                        </li>
-                        <li className="nav-item"> 
-                            <Link className="nav-link" to="/tutores">Tutores</Link> 
-                        </li>
-                        <li className="nav-item dropdown"> 
-                            <Link
-                                className="nav-link dropdown-toggle" 
-                                to="/" 
-                                id="navbarDropdown1" 
-                                role="button" 
-                                data-toggle="dropdown" 
-                                aria-haspopup="true" 
-                                aria-expanded="false"> 
-                                Cursos 
-                            </Link>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdown1"> 
-                                <Link className="dropdown-item" to="/ciencias">Ciencias</Link> 
-                                <Link className="dropdown-item" to="/diseño">Diseño</Link>
-                                <div className="dropdown-divider"></div>
-                                <Link className="dropdown-item" to="/mecatronica">Mecatronica</Link> 
-                            </div>
-                        </li>
-                        <li className="nav-item mr-5"> 
-                            <Link className="nav-link" to="/acerca-de">Información</Link> 
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            <header>
+                <nav className="navbar navbar-expand-lg navbar-light bg-white">
+                    <Link to="/">
+                        <img src="https://via.placeholder.com/270x60?text=Logo+Expertok" className="navbar-brand" alt="Logo navbar"/>
+                    </Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span className="navbar-toggler-icon"></span> </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent1">
+                        <ul className="navbar-nav ml-auto mr-2">
+                            <li className="nav-item active"> 
+                                <Link className="nav-link" to="/">Inicio</Link> 
+                            </li>
+                            <li className="nav-item"> 
+                                <Link className="nav-link" to="/tutores">Tutores</Link> 
+                            </li>
+                            <li className="nav-item dropdown"> 
+                                <Link
+                                    className="nav-link dropdown-toggle" 
+                                    to="/" 
+                                    id="navbarDropdown1" 
+                                    role="button" 
+                                    data-toggle="dropdown" 
+                                    aria-haspopup="true" 
+                                    aria-expanded="false"> 
+                                    Cursos 
+                                </Link>
+                                <div className="dropdown-menu" aria-labelledby="navbarDropdown1"> 
+                                    <Link className="dropdown-item" to="/ciencias">Ciencias</Link> 
+                                    <Link className="dropdown-item" to="/diseño">Diseño</Link>
+                                    <div className="dropdown-divider"></div>
+                                    <Link className="dropdown-item" to="/mecatronica">Mecatronica</Link> 
+                                </div>
+                            </li>
+                            <li className="nav-item mr-5"> 
+                                <Link className="nav-link" to="/acerca-de">Información</Link> 
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
             <Switch>
                 <Route path="/" exact>
                     <Home />
@@ -67,7 +74,7 @@ function App() {
                     <Home />
                 </Route>
                 <Route path="/acerca-de">
-                    <Home />
+                    <Upload />
                 </Route>
             </Switch>
             
--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
-import './css/home.css';
-import banner from './img/banner.jpg';
+import './css/home.index.css';
+import './css/home.banner.css' ;
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
     render() {
         return (
             <div className="container-fluid m-0 p-0">
-                <div className="row m-0 p-0">
-                    <img className="vh-100 vw-100 w-100 m-0 p-0" src={banner} alt="banner"/>
+                <div className="banner">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12 col-sm-6 banner-text">
+                                <h2>Tutores certificados a tu alcance</h2>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur rerum, ea fuga veniam blanditiis quo.</p>
+                                <Link className="link" to="/acerca-de">Acerca de</Link>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="row m-0 p-0 pt-5">
-                    <div className="col-12 col-md-7 col-lg-9 m-0 p-0 mt-2 px-5 mb-5">
+                    <div className="col-12  m-0 p-0 mt-2 px-5 mb-5">
                         <div className="section description">
                             <h1 className="w-100 col-12 m-0 p-0 text-center mb-4 text-primary">
                                 ¿Qué es Expertok?
@@ -19,77 +28,127 @@ export default class Home extends Component {
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi reiciendis aliquid corporis rem sed eaque veniam culpa molestiae accusamus totam quos eligendi placeat maiores, est minima iste dignissimos earum natus deserunt inventore! Id obcaecati ab quia nihil consequuntur, deserunt, qui aliquid corrupti voluptates incidunt dolore esse nam. Temporibus delectus nesciunt inventore excepturi voluptatibus, assumenda eos, reiciendis repellat quasi hic consequuntur voluptas non eaque laudantium consequatur dolorum facilis modi illo sequi. Doloribus delectus itaque repellendus obcaecati aspernatur voluptates ab beatae adipisci mollitia in dolorem neque doloremque perferendis nisi impedit corporis amet nemo fugit voluptatibus deserunt, temporibus nulla expedita similique. Eaque, minus.
                             </p>
                         </div>
-                        <div className="section vision">
-                            <h1 className="w-100 col-12 m-0 p-0 text-center my-4 text-primary">
-                                ¿A qué cursos puedo acceder?
-                            </h1>        
-                            <div className="list-group">
-                                <button type="button" className="list-group-item list-group-item-action active text-center">
-                                    Primaria/Secundaria
-                                </button>
-                                <button type="button" className="list-group-item list-group-item-action">Fisica</button>
-                                <button type="button" className="list-group-item list-group-item-action">Matematicas</button>
-                                <button type="button" className="list-group-item list-group-item-action">Ingles</button>
-                                <button type="button" className="list-group-item list-group-item-action">Mas+</button>
-                            </div>
-
-                            <div className="list-group mt-5">
-                                <button type="button" className="list-group-item list-group-item-action active text-center">
-                                    Fortalecimiento
-                                </button>
-                                <button type="button" className="list-group-item list-group-item-action">Mecatronica</button>
-                                <button type="button" className="list-group-item list-group-item-action">Diseño grafico</button>
-                                <button type="button" className="list-group-item list-group-item-action">Programacion</button>
-                                <button type="button" className="list-group-item list-group-item-action" >Mas+</button>
-                            </div>
-                            <div className="list-group mt-5">
-                                <button type="button" className="list-group-item list-group-item-action active text-center">
-                                    Asesorias
-                                </button>
-                                <button type="button" className="list-group-item list-group-item-action">Fortalecimiento cientifico-tecnico</button>
-                                <button type="button" className="list-group-item list-group-item-action">Fortalecimiento Artes</button>
-                                <button type="button" className="list-group-item list-group-item-action">Fortalecimiento s</button>
-                                <button type="button" className="list-group-item list-group-item-action" >Mas+</button>
-                            </div>
+                </div>
+                <div className="row w-100 m-0 p-0">
+                    <h1 className="w-100  p-0 text-center my-4 text-primary">
+                        ¿A qué cursos puedo acceder?
+                    </h1>        
+                    <div className="row w-100 m-0 p-0 d-flex">
+                        <div className="list-group col-12 col-md-4 m-0 p-3">
+                            <button 
+                                type="button" 
+                                className="list-group-item list-group-item-action active text-center">
+                                Primaria/Secundaria
+                            </button>
+                            <button 
+                                type="button" 
+                                className="list-group-item list-group-item-action">
+                                Fisica
+                            </button>
+                            <button 
+                                type="button"
+                                className="list-group-item list-group-item-action">
+                                Matematicas
+                            </button>
+                            <button 
+                                type="button"
+                                className="list-group-item list-group-item-action">
+                                Ingles
+                            </button>
+                            <button
+                                type="button" 
+                                className="list-group-item list-group-item-action">
+                                Mas+
+                            </button>
                         </div>
-                    </div>
-                    <div className="col-12 col-md-5 col-lg-3 p-1 m-0">
-                        <div className="row w-100 m-0">
-                            <h1 className="w-100 text-center">Fundadores</h1>
-                            <div className="col-12 col-md-12 m-0 py-2">
-                                <div className="card card-fundadores m-0">
-                                    <img src="https://dummyimage.com/500x300/fff/aaa" className="card-img-top" alt="..." />
-                                    <div className="card-body">
-                                        <h5 className="card-title">Card title</h5>
-                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-12 col-sm-12 m-0 py-2">
-                                <div className="card card-fundadores m-0">
-                                    <img src="https://dummyimage.com/500x300/fff/aaa" className="card-img-top" alt="..." />
-                                    <div className="card-body">
-                                        <h5 className="card-title">Card title</h5>
-                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-12 col-sm-12 m-0 py-2">
-                                <div className="card card-fundadores m-0">
-                                    <img src="https://dummyimage.com/500x300/fff/aaa" className="card-img-top" alt="..." />
-                                    <div className="card-body">
-                                        <h5 className="card-title">Card title</h5>
-                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>  
+                        <div className="list-group col-12 col-md-4 m-0 p-3">
+                            <button 
+                                type="button" 
+                                className="list-group-item list-group-item-action active text-center">
+                                Primaria/Secundaria
+                            </button>
+                            <button 
+                                type="button" 
+                                className="list-group-item list-group-item-action">
+                                Fisica
+                            </button>
+                            <button 
+                                type="button"
+                                className="list-group-item list-group-item-action">
+                                Matematicas
+                            </button>
+                            <button 
+                                type="button"
+                                className="list-group-item list-group-item-action">
+                                Ingles
+                            </button>
+                            <button
+                                type="button" 
+                                className="list-group-item list-group-item-action">
+                                Mas+
+                            </button>
+                        </div>
+                        <div className="list-group col-12 col-md-4 m-0 p-3">
+                            <button 
+                                type="button" 
+                                className="list-group-item list-group-item-action active text-center">
+                                Primaria/Secundaria
+                            </button>
+                            <button 
+                                type="button" 
+                                className="list-group-item list-group-item-action">
+                                Fisica
+                            </button>
+                            <button 
+                                type="button"
+                                className="list-group-item list-group-item-action">
+                                Matematicas
+                            </button>
+                            <button 
+                                type="button"
+                                className="list-group-item list-group-item-action">
+                                Ingles
+                            </button>
+                            <button
+                                type="button" 
+                                className="list-group-item list-group-item-action">
+                                Mas+
+                            </button>
+                        </div>
+
                     </div>
                 </div>
             </div>
+            <div className="row w-100 m-0 my-5">
+                <h1 className="w-100 text-center my-5">Fundadores</h1>
+                <div className="row w-100 d-flex justify-content-around m-0 p-0">
+                    <div className="card card-fundadores col-12 col-md-4 col-lg-3">
+                        <img src="https://dummyimage.com/500x300/fff/aaa" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Dilan Ariza</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <Link to="/" className="btn btn-primary btn-block">ver perfil</Link>
+                        </div>
+                    </div>
+                    <div className="card card-fundadores col-12 col-md-4 col-lg-3">
+                        <img src="https://dummyimage.com/500x300/fff/aaa" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Dilan Ariza</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <Link to="/" className="btn btn-primary btn-block">ver perfil</Link>
+                        </div>
+                    </div>
+                    <div className="card card-fundadores col-12 col-md-4 col-lg-3">
+                        <img src="https://dummyimage.com/500x300/fff/aaa" className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title text-center">Dilan Ariza</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <Link to="/" className="btn btn-primary btn-block">ver perfil</Link>
+                        </div>
+                    </div>
+                </div>                    
+            </div>
+        </div>
         )
     }
 }
